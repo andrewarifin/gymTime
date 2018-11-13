0bbcd6105749b529a71a5520f19df8b7f8a4d8a8
Andrew Arifin <andrewarifin@Andrews-MacBook-Pro-2.local>
2018-11-13 13:42:33 -0800

    create page functionality

--- js/create_page.js
+++ js/create_page.js
@@ -4,8 +4,18 @@ document.addEventListener('DOMContentLoaded', function() {
 
     var timepicker = document.querySelectorAll('.timepicker');
     var instances = M.Timepicker.init(timepicker);
+
+    var timeoutPicker = document.querySelectorAll('.timeoutpicker');
+    var timeOutOptions = {
+      'onCloseEnd': show_exercises
+    }
+    var instances = M.Timepicker.init(timeoutPicker, timeOutOptions);
 });
 
 function show_cart() {
   console.log('hello');
+}
+
+function show_exercises() {
+  $("#availableExercises").show()
 }
