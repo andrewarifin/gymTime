d79af9246af33993ef58ab6eabb23c037808d1b3
akpham97 <apham1109@ucla.edu>
2018-12-03 12:07:19 -0800

    fix

--- login.js
+++ login.js
@@ -31,7 +31,7 @@ function login(){
 function logout() {
 	firebase.auth().signOut().then(function() {
 	  // Sign-out successful.
-	  window.location.href = 'https://andrewarifin.github.io/gymTime/login.html';
+	  window.location.href = 'https://andrewarifin.github.io/gymTime';
 	}).catch(function(error) {
 	  // An error happened.
 	  window.alert(error)
